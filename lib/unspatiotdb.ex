11a92edefb244907cc3144fb464869794263a147
Santiago Núñez-Corrales <snunezcr@gmail.com>
2019-09-22 16:53:02 -0500

    Mix configuration

---
+++ lib/unspatiotdb.ex
@@ -0,0 +1,18 @@
+defmodule Usdb do
+  @moduledoc """
+  Documentation for Usdb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Usdb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/usdb_test.exs
@@ -0,0 +1,8 @@
+defmodule UsdbTest do
+  use ExUnit.Case
+  doctest Usdb
+
+  test "greets the world" do
+    assert Usdb.hello() == :world
+  end
+end
